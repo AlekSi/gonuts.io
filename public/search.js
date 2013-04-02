b2ca2cb6d0a40c8ac87876340477f912e681cc54
Alexey Palazhchenko <alexey.palazhchenko@gmail.com>
2013-04-02 13:01:32 +0400

    Remove April Fools' jokes.

--- public/search.js
+++ public/search.js
@@ -29,21 +29,3 @@ function bindSearchEvents() {
 }
 
 bindEvent(window, 'load', bindSearchEvents);
-
-function dance(id, min, max) {
-  var e = document.getElementById(id);
-  if (e == null) {
-    return;
-  }
-  setInterval(function() {
-    e.textContent = Math.floor(Math.random() * (max - min + 1)) + min;
-  }, 2000)
-}
-
-function startFun() {
-  dance("version-count", 50000, 100000)
-  dance("nuts-count", 5000, 10000)
-  dance("users-count", 10000, 50000)
-}
-
-bindEvent(window, 'load', startFun);
